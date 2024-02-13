5a674cad106b5110ef164589897178581e9efebf
Zach Brown <bbrown64506@gmail.com>
2024-02-13 09:36:14 -0600

    adding wsgi.py file

--- run.py
+++ run.py
@@ -1,25 +1,31 @@
-from flask import Flask
-from src.database.firebase_config import initialize_firebase_app
-
-# Routes
-from src.routes.base import base_bp
-from src.routes.user_router import users_bp
+from argparse import ArgumentParser
+from src.app import create_app
 
 
-def create_app(test_config=None):
-    initialize_firebase_app()
+def create_parser():
+    parser = ArgumentParser(
+        description="Run the TapRx Backend API"
+    )
 
-    app = Flask(__name__, instance_relative_config=True)
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help="Enable debug mode"
+    )
 
-    if test_config:
-        app.config.update(test_config)
+    parser.add_argument(
+        "--use_reloader",
+        action="store_true",
+        help="Enable the reloader"
+    )
 
-    app.register_blueprint(base_bp)
-    app.register_blueprint(users_bp, url_prefix='/users')
-
-    return app
+    return parser
 
 
 if __name__ == '__main__':
+    parser = create_parser()
+
+    args = parser.parse_args()
+
     app = create_app()
-    app.run(debug=True)  # Turn off debug mode in production
+    app.run(debug=args.debug, use_reloader=args.use_reloader)
---
+++ src/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+from src.database.firebase_config import initialize_firebase_app
+from src.routes.base import base_bp
+from src.routes.user_router import users_bp
+
+
+def create_app(test_config=None):
+    initialize_firebase_app()
+
+    app = Flask(__name__, instance_relative_config=True)
+
+    if test_config:
+        app.config.update(test_config)
+
+    app.register_blueprint(base_bp)
+    app.register_blueprint(users_bp, url_prefix='/users')
+
+    return app
