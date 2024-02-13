f70102ba9eeb2a5b6df0ddffc33f03d3a6090494
Matthew Schutz <schutzmw3@gmail.com>
2024-02-12 23:48:17 -0600

    Added endpoint for creating and updating a user.

--- src/routes/user_router.py
+++ src/routes/user_router.py
@@ -1,9 +1,8 @@
-from typing import Optional
-
 from firebase_admin import db, exceptions
 from flask import Blueprint, current_app, jsonify, request
 
 from src.database import firebase_service
+from src.models.User import User
 from src.routes.auth import firebase_auth_required
 
 users_bp = Blueprint('users_bp', __name__)
@@ -22,15 +21,10 @@ def get_user(user_id):
 
 @users_bp.route('/', methods=['POST'])
 @firebase_auth_required
-def add_user():
+def modify_user():
     db_users_ref = db.reference("/users")
     try:
         user_id = request.json["user_id"]
-        first_name = request.json["first_name"]
-        last_name = request.json["last_name"]
-
-        # Not required for registration
-        phone = request.json.get("phone", None)
     except (ValueError, KeyError) as ex:
         current_app.logger.error(f"Invalid request JSON: {ex}")
         return jsonify({"message": "Invalid request"}), 400
@@ -39,66 +33,94 @@ def add_user():
     if user_id != requesting_user_id:
         return jsonify({"message": "Insufficient permissions"}), 403
 
-    try:
-        store_user(db_users_ref, user_id, first_name, last_name, phone)
-    except (ValueError, TypeError, exceptions.FirebaseError):
-        return jsonify({"message": "Internal server error"}), 500
-    except UserAlreadyExistsError:
-        return jsonify({"message": "User already exists"}), 400
+    if firebase_service.get_node(db_users_ref, user_id) is None:
+        try:
+            create_user(db_users_ref, user_id, request.json)
+        except InvalidRequestError:
+            return jsonify({"message": "Invalid request"}), 400
+        except (ValueError, TypeError, exceptions.FirebaseError) as ex:
+            return jsonify({"message": "Failed to create user"}), 500
 
-    return jsonify({"message": "User created"}), 201
+        return jsonify({"message": "User created"}), 201
+    else:
+        try:
+            update_user(db_users_ref, user_id, request.json)
+        except (ValueError, TypeError, exceptions.FirebaseError) as ex:
+            return jsonify({"message": "Failed to update user"}), 500
+
+        return jsonify({"message": "User updated"}), 200
 
 
-class UserAlreadyExistsError(Exception):
-    def __init__(self, message="User already exists"):
-        self.message = message
-        super().__init__(self.message)
-
-
-def store_user(
-        db_user_ref: db.Reference, user_id: str, first_name: str, last_name: str, phone: Optional[str]
-) -> None:
+def create_user(db_users_ref: db.Reference, user_id: str, json_dict: dict) -> None:
     """
-    Stores user item in database. Includes empty arrays that can be filled later.
+    Creates a new user in the database.
 
     Args:
-        db_user_ref: (firebase_admin.db.Reference) Reference to the database.
+        db_users_ref: (firebase_admin.db.Reference) Reference to the database.
         user_id: (str) Username for user.
-        first_name: (str) first name for user.
-        last_name: (str) last name for user.
-        phone: (Optional[str]) phone number for user.
+        json_dict: (dict) Dictionary containing user data.
 
     Returns:
         None
 
     Raises:
-        ValueError: If the JSON object is None or if any parameters are None.
-        TypeError: If the JSON object is not a dictionary.
-        firebase_admin.exceptions.FirebaseError: If an error occurs while interacting with the database.
+        InvalidRequestError: If the request is invalid.
+        ValueError, TypeError, exceptions.FirebaseError: If an error occurs while trying to store the user.
     """
 
-    if firebase_service.get_node(db_user_ref, user_id) is not None:
-        current_app.logger.error(f"User {user_id} already exists in the database")
-        raise UserAlreadyExistsError
+    try:
+        first_name = json_dict["first_name"]
+        last_name = json_dict["last_name"]
+
+        # Not required for registration
+        phone = json_dict.get("phone", None)
+    except (ValueError, KeyError) as ex:
+        current_app.logger.error(f"Invalid request JSON: {ex}")
+        raise InvalidRequestError
+
+    new_user = User(user_id=user_id, first_name=first_name, last_name=last_name, phone=phone)
 
     try:
-        user_data = {
-            'first_name': first_name,
-            'last_name': last_name,
-            'medications': [],
-            'dependents': [],
-            'monitored_by': [],
-            'monitoring': []
-        }
-        if phone:
-            user_data['phone'] = phone
-        db_user_ref.child(user_id).set(user_data)
-    except ValueError as ex:
-        current_app.logger.error(f"Invalid JSON object: {ex}")
+        firebase_service.add_node(db_users_ref, new_user.user_id, new_user.to_dict())
+    except (ValueError, TypeError, exceptions.FirebaseError) as ex:
+        current_app.logger.error(f"Failed to create user: {ex}")
         raise ex
-    except TypeError as ex:
-        current_app.logger.error(f"Invalid JSON object type: {ex}")
+
+
+def update_user(db_users_ref: db.Reference, user_id: str, json_dict: dict) -> None:
+    """
+    Updates an existing user in the database.
+
+    Args:
+        db_users_ref: (firebase_admin.db.Reference) Reference to the database.
+        user_id: (str) Username for user.
+        json_dict: (dict) Dictionary containing user data to be updated.
+
+    Returns:
+        None
+
+    Raises:
+        ValueError, TypeError, exceptions.FirebaseError: If an error occurs while trying to update the user.
+    """
+    try:
+        user_dict = firebase_service.get_node(db_users_ref, user_id)
+    except (ValueError, exceptions.FirebaseError) as ex:
+        current_app.logger.error(f"Failed to retrieve user: {ex}")
         raise ex
-    except exceptions.FirebaseError as ex:
-        current_app.logger.error(f"Firebase error: {ex}")
+
+    updated_user = User.from_dict(user_dict)
+    updated_user.first_name = json_dict.get('first_name', updated_user.first_name)
+    updated_user.last_name = json_dict.get('last_name', updated_user.last_name)
+    updated_user.phone = json_dict.get('phone', updated_user.phone)
+
+    try:
+        firebase_service.add_node(db_users_ref, updated_user.user_id, updated_user.to_dict())
+    except (ValueError, TypeError, exceptions.FirebaseError) as ex:
+        current_app.logger.error(f"Failed to update user: {ex}")
         raise ex
+
+
+class InvalidRequestError(Exception):
+    def __init__(self, message="Invalid request"):
+        self.message = message
+        super().__init__(self.message)
