574eeb6fdaa5e10819c3d96e35db6ac8bc06df7f
Zach Brown <bbrown64506@gmail.com>
2024-02-15 11:23:48 -0600

    Removing invalid logging on firebase_config.py (current_app.logger was throwing weird errors). If an exception is raised, it's traceback will be logged by default.

--- src/database/firebase_config.py
+++ src/database/firebase_config.py
@@ -3,7 +3,6 @@ import os
 import firebase_admin
 from dotenv import load_dotenv
 from firebase_admin import credentials
-from flask import current_app
 
 FIREBASE_CREDENTIALS_PATH = "FIREBASE_CREDENTIALS_PATH"
 FIREBASE_DB_URL = "FIREBASE_DB_URL"
@@ -19,20 +18,6 @@ if os.getenv(FIREBASE_DB_URL) is None:
 def initialize_firebase_app() -> None:
     if firebase_admin._apps:
         raise ValueError("Firebase app already initialized")
-    try:
-        cred = credentials.Certificate(os.getenv(FIREBASE_CREDENTIALS_PATH))
-    except IOError as ex:
-        current_app.logger.error(f"Failed to read the certificate: {ex}")
-        raise ex
-    except ValueError as ex:
-        current_app.logger.error(f"Invalid certificate: {ex}")
-        raise ex
 
-    try:
-        firebase_admin.initialize_app(cred, {
-            "databaseURL": os.getenv(FIREBASE_DB_URL)
-        })
-    except ValueError as ex:
-        current_app.logger.error(f"Invalid app: {ex}")
-        raise ex
-    print("Firebase app initialized")
+    cred = credentials.Certificate(os.getenv(FIREBASE_CREDENTIALS_PATH))
+    firebase_admin.initialize_app(cred, {"databaseURL": os.getenv(FIREBASE_DB_URL)})
