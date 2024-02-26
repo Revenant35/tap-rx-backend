e1d4c1b7b8179b97d718f33c110d507b36d897b5
Zach Brown <bbrown64506@gmail.com>
2024-02-26 09:11:46 -0600

    Adding flasgger doc-generation

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+import yaml
+from flasgger import Swagger
 from flask import Flask
 
 from src.database.firebase_config import initialize_firebase_app
@@ -7,13 +9,50 @@ from src.routes.medication_router import medications_bp
 from src.routes.user_router import users_bp
 
 
-def create_app(test_config=None):
+def read_yaml_file(file_path: str):
+    """
+    Reads a YAML file and returns its contents.
+    Args:
+        file_path: (str) The path to the YAML file.
+
+    Returns:
+        dict: The contents of the YAML file.
+    """
+    with open(file_path, 'r') as file:
+        return yaml.safe_load(file)
+
+
+def init_swagger(app: Flask):
+    """
+    Initializes the Swagger UI for the Flask app.
+
+    Args:
+        app: (Flask) The Flask app to initialize Swagger for.
+    """
+    models = read_yaml_file("src/documentation/models.yaml")
+    swagger_config = {
+        "headers": [],
+        "specs": [
+            {
+                "endpoint": 'apispec_1',
+                "route": '/apispec_1.json',
+                "rule_filter": lambda rule: True,
+                "model_filter": lambda tag: True,
+            }
+        ],
+        "static_url_path": "/flasgger_static",
+        "swagger_ui": True,
+        "specs_route": "/apidocs/",
+        "definitions": models  # Add your loaded models here
+    }
+    swagger = Swagger(app, config=swagger_config)
+
+
+def create_app():
     initialize_firebase_app()
 
     app = Flask(__name__, instance_relative_config=True)
-
-    if test_config:
-        app.config.update(test_config)
+    init_swagger(app)
 
     app.register_blueprint(base_bp)
     app.register_blueprint(medications_bp, url_prefix='/medications')
--- src/routes/user_router.py
+++ src/routes/user_router.py
@@ -10,6 +10,48 @@ users_bp = Blueprint('users_bp', __name__)
 @users_bp.route('/', methods=['GET'])
 @firebase_auth_required
 def handle_get_users():
+    """
+    Retrieve a list of users
+    ---
+    tags:
+        - users
+    parameters:
+      - name: start_at
+        in: query
+        type: string
+        required: false
+        description: UID to start at, for cursor-based pagination
+      - name: limit
+        in: query
+        type: integer
+        required: false
+        description: Number of users to retrieve, max limit of 50
+        default: 50
+    responses:
+      200:
+        description: A list of users and the UID for the next page
+        schema:
+          type: object
+          properties:
+            success:
+              type: boolean
+              description: The status of the response
+            message:
+              type: string
+              description: The message of the response
+            data:
+              type: array
+              description: The list of users
+              items:
+                $ref: '#/definitions/User'
+            next_page:
+              type: string
+              description: The UID for the next page
+      401:
+        description: Unauthorized
+      500:
+        description: Failed to fetch users
+    """
     # Fetch query parameters for pagination
     start_at = request.args.get('start_at', None)  # UID to start at, for cursor-based pagination
     limit = min(int(request.args.get('limit', 50)), 50)  # Enforce a max limit of 50
@@ -34,6 +76,42 @@ def handle_get_users():
 @users_bp.route('/<user_id>', methods=['GET'])
 @firebase_auth_required
 def handle_get_user(user_id):
+    """
+    Retrieve a specific user
+    ---
+    tags:
+      - users
+    parameters:
+      - name: user_id
+        in: path
+        type: string
+        required: true
+        description: The UID of the user
+    responses:
+      200:
+        description: A list of users and the UID for the next page
+        schema:
+          type: object
+          properties:
+            success:
+              type: boolean
+              description: The status of the response
+              value: true
+            message:
+              type: string
+              description: The message of the response
+            data:
+              type: dict
+              description: The found user
+              items:
+                $ref: '#/definitions/User'
+      401:
+        description: Unauthorized
+      404:
+        description: User not found
+      500:
+        description: Failed to fetch user
+    """
     user_verified, error_response = verify_user(user_id, request)
     if not user_verified:
         return error_response
@@ -58,6 +136,45 @@ def handle_get_user(user_id):
 @firebase_auth_required
 @validate_json("user_id", "first_name", "last_name")
 def handle_create_user():
+    """
+    Create a new user
+    ---
+    tags:
+      - users
+    parameters:
+      - in: body
+        name: body
+        required: true
+        schema:
+         $ref: '#/definitions/User'
+    responses:
+      201:
+        description: User created successfully
+        schema:
+          type: object
+          properties:
+            success:
+              type: boolean
+              description: The status of the response
+            message:
+              type: string
+              description: The message of the response
+            data:
+              type: dict
+              description: The created user
+              items:
+                $ref: '#/definitions/User'
+
+      400:
+        description: Invalid request
+      401:
+        description: Unauthorized
+      409:
+        description: User already exists
+      500:
+        description: Internal server error
+    """
+
     user_id = request.json["user_id"]
     user_verified, error_response = verify_user(user_id, request)
     if not user_verified:
@@ -82,7 +199,48 @@ def handle_create_user():
 @users_bp.route('/<user_id>', methods=['PUT'])
 @firebase_auth_required
 def handle_update_user(user_id):
-
+    """
+    Updates a user
+    ---
+    tags:
+      - users
+    parameters:
+      - name: user_id
+        in: path
+        type: string
+        required: true
+        description: The UID of the user
+      - in: body
+        name: body
+        required: true
+        schema:
+          $ref: '#/definitions/User'
+    responses:
+      200:
+        description: User updated successfully
+        schema:
+          type: object
+          properties:
+            success:
+              type: boolean
+              description: The status of the response
+            message:
+              type: string
+              description: The message of the response
+            data:
+              type: dict
+              description: The updated user
+              items:
+                $ref: '#/definitions/User'
+      400:
+        description: Invalid request
+      401:
+        description: Unauthorized
+      409:
+        description: User already exists
+      500:
+        description: Internal server error
+    """
     user_verified, error_response = verify_user(user_id, request)
     if not user_verified:
         return error_response
@@ -100,4 +258,3 @@ def handle_update_user(user_id):
             "message": "Failed to update user",
             "error": str(e)
         }), 500
-
