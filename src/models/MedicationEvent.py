7f65f177775a0b4487186c329868bce36bd6498a
Matthew Schutz <schutzmw3@gmail.com>
2024-03-06 23:58:05 -0600

    Including medication event model

---
+++ src/models/MedicationEvent.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+
+
+class MedicationEvent:
+    medication_event_id: str
+    medication_id: str
+    timestamp: datetime
+    dosage: str | None
+
+    def __init__(self, medication_event_id: str, medication_id: str, timestamp: datetime, dosage: str | None = None):
+        """
+        Initialize a new medication event object
+
+        Args:
+            medication_event_id: {str} The medication event's ID.
+            medication_id: {str} The medication's ID.
+            timestamp: {datetime} The event's timestamp.
+            dosage: {str} The event's dosage.
+        """
+        self.medication_event_id = medication_event_id
+        self.medication_id = medication_id
+        self.timestamp = timestamp
+        self.dosage = dosage
+
+    def __eq__(self, other):
+        return all(getattr(self, attr) == getattr(other, attr) for attr in vars(self))
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__} medication_id=({self.medication_id}) timestamp=({self.timestamp}), dosage=({self.dosage})>'
+
+    @staticmethod
+    def from_dict(data: dict):
+        return MedicationEvent(
+            medication_event_id=data["medication_event_id"],
+            medication_id=data["medication_id"],
+            timestamp=data["timestamp"],
+            dosage=data.get("dosage", None)
+        )
+
+    def to_dict(self):
+        return {
+            "medication_event_id": self.medication_event_id,
+            "medication_id": self.medication_id,
+            "timestamp": self.timestamp.isoformat(),
+            "dosage": self.dosage
+        }
